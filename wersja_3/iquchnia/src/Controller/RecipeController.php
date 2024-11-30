e0ba973397465fe331ed067fa0b715b2b979fffc
Dawid.zielaskowski <dawidek200108@gmail.com>
2024-11-30 12:07:10 +0100

    wersja 3 połączona z wersją 2

--- wersja_3/iquchnia/src/Controller/RecipeController.php
+++ wersja_3/iquchnia/src/Controller/RecipeController.php
@@ -11,29 +11,47 @@ use Symfony\Component\Routing\Annotation\Route;
 class RecipeController extends AbstractController
 {
     #[Route('/recipes', name: 'recipe_list')]
-    public function list(RecipeRepository $recipeRepository): Response
+    public function list(Request $request, RecipeRepository $recipeRepository): Response
     {
-        // Pobieramy wszystkie przepisy z bazy danych
-        $recipes = $recipeRepository->findAll();
+        // Pobranie frazy wyszukiwania z zapytania GET
+        $search = $request->query->get('search');
 
-        // Przekazujemy przepisy do widoku
+        // Jeśli fraza wyszukiwania jest obecna, przeszukaj przepisy
+        if ($search) {
+            $recipes = $recipeRepository->findBySearch($search);
+        } else {
+            // Jeśli brak frazy, pobierz wszystkie przepisy
+            $recipes = $recipeRepository->findAll();
+        }
+
+        // Przekazujemy przepisy oraz frazę wyszukiwania do widoku
         return $this->render('recipe/index.html.twig', [
             'recipes' => $recipes,
+            'search' => $search,  // Wartość frazy wyszukiwania
         ]);
     }
-    #[Route('/recipes/filter', name: 'recipe_filter')]
+
+    #[Route('/filter', name: 'recipe_filter')]
     public function filter(Request $request, RecipeRepository $recipeRepository): Response
     {
+        // Odczytanie parametrów z zapytania GET
         $criteria = [
-            'cuisine' => $request->query->get('cuisine'),
-            'difficulty' => $request->query->get('difficulty'),
-            'is_vegetarian' => $request->query->get('is_vegetarian'),
+            'cuisine' => $request->query->get('cuisine') ?: null,
+            'difficulty' => $request->query->get('difficulty') ?: null,
+            'is_vegetarian' => $request->query->get('is_vegetarian') ?: null,
         ];
 
-        $recipes = $recipeRepository->findByFilters($criteria);
+        // Pobranie frazy wyszukiwania
+        $search = $request->query->get('search');
 
+        // Znalezienie przepisów na podstawie filtrów i wyszukiwania
+        $recipes = $recipeRepository->findByFiltersAndSearch($criteria, $search);
+
+        // Renderowanie szablonu z przepisami i frazą wyszukiwania
         return $this->render('recipe/filter.html.twig', [
             'recipes' => $recipes,
+            'search' => $search,  // Wartość wyszukiwania
         ]);
     }
+
 }
--- wersja_3/iquchnia/src/Repository/RecipeRepository.php
+++ wersja_3/iquchnia/src/Repository/RecipeRepository.php
@@ -6,9 +6,6 @@ use App\Entity\Recipe;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
-/**
- * @extends ServiceEntityRepository<Recipe>
- */
 class RecipeRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
@@ -16,23 +13,39 @@ class RecipeRepository extends ServiceEntityRepository
         parent::__construct($registry, Recipe::class);
     }
 
-    public function findByFilters(array $criteria): array
+    // Metoda wyszukiwania po frazie
+    public function findBySearch(string $search): array
+    {
+        return $this->createQueryBuilder('r')
+            ->where('r.name LIKE :search OR r.description LIKE :search') // Wyszukiwanie po nazwie i opisie
+            ->setParameter('search', '%' . $search . '%')
+            ->getQuery()
+            ->getResult();
+    }
+
+    // Metoda do filtrów (można ją już mieć)
+    public function findByFiltersAndSearch(array $criteria, ?string $search): array
     {
         $qb = $this->createQueryBuilder('r');
 
-        if (isset($criteria['cuisine'])) {
+        // Dodajemy warunki filtrów
+        if ($criteria['cuisine']) {
             $qb->andWhere('r.cuisine = :cuisine')
                 ->setParameter('cuisine', $criteria['cuisine']);
         }
-
-        if (isset($criteria['difficulty'])) {
+        if ($criteria['difficulty']) {
             $qb->andWhere('r.difficulty = :difficulty')
                 ->setParameter('difficulty', $criteria['difficulty']);
         }
+        if ($criteria['is_vegetarian'] !== null) {
+            $qb->andWhere('r.isVegetarian = :is_vegetarian')
+                ->setParameter('is_vegetarian', $criteria['is_vegetarian']);
+        }
 
-        if (isset($criteria['isVegetarian'])) {
-            $qb->andWhere('r.is_vegetarian = :is_vegetarian')
-                ->setParameter('is_vegetarian', (bool) $criteria['is_vegetarian']);
+        // Dodajemy warunek wyszukiwania
+        if ($search) {
+            $qb->andWhere('r.name LIKE :search OR r.description LIKE :search')
+                ->setParameter('search', '%' . $search . '%');
         }
 
         return $qb->getQuery()->getResult();
